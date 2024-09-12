50597c5a34eaf34d401d86e5c012c54ab510406b
Ahram Kim <45866137+byahram@users.noreply.github.com>
2024-09-12 16:05:11 +0900

    섹션 1: Intro + 환경세팅

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,20 @@
-import type { Metadata } from 'next'
-import './globals.scss'
-import Gnb from './gnb'
+import type { Metadata } from "next";
+import "./globals.scss";
+import Nav from "./nav";
 
 export const metadata: Metadata = {
-  title: 'UI요소모음 | FE재남',
-  description: 'Vanilla / React로 UI요소 만들기',
-}
+  title: "React UI Components",
+  description: "[React / VanillaJS] UI 요소 직접 만들기",
+};
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <html lang="ko">
       <body>
-        <Gnb />
+        <Nav />
         <main>{children}</main>
       </body>
     </html>
-  )
-}
-export default Layout
+  );
+};
+export default Layout;
---
+++ src/app/nav.tsx
@@ -0,0 +1,96 @@
+"use client";
+
+import {
+  ROUTE_PATH,
+  ChildRoute,
+  ParentRoute,
+  navRootList,
+  isParentRoute,
+  routes,
+  ROUTE,
+} from "@/routes";
+import Link from "next/link";
+import classNames from "classnames";
+import { useParams } from "next/navigation";
+
+// ParentNavItem
+const ParentNavItem = ({
+  route: { name, link, children },
+  currentPath,
+}: {
+  route: ParentRoute;
+  currentPath: ROUTE_PATH;
+}) => {
+  const open = children.includes(currentPath);
+
+  return (
+    <li className={classNames("parent", `items-${children.length}`, { open })}>
+      <Link href={link}>{name}</Link>
+      <ul className="subRoutes">
+        {children.map((r) => {
+          const route = routes[r];
+          return (
+            <NavItem route={route} currentPath={currentPath} key={route.key} />
+          );
+        })}
+      </ul>
+    </li>
+  );
+};
+
+// ChildNavItem
+const ChildNavItem = ({
+  route: { name, link, children },
+  currentPath,
+}: {
+  route: ChildRoute;
+  currentPath: ROUTE_PATH;
+}) => {
+  return (
+    <li
+      className={classNames({
+        active: link === currentPath,
+        disabled: !children,
+      })}
+    >
+      {children ? <Link href={link}>{name}</Link> : name}
+    </li>
+  );
+};
+
+// NavItem
+const NavItem = ({
+  route,
+  currentPath,
+}: {
+  route: ROUTE;
+  currentPath: ROUTE_PATH;
+}) => {
+  if (isParentRoute(route)) {
+    return <ParentNavItem route={route} currentPath={currentPath} />;
+  } else {
+    return <ChildNavItem route={route} currentPath={currentPath} />;
+  }
+};
+
+const Nav = () => {
+  const { item = [] } = useParams();
+  const currentPath = ["", ...item].join("/") as ROUTE_PATH;
+
+  return (
+    <aside>
+      <h1>
+        <Link href="/">
+          UI 요소 모음 <sub>FE재남</sub>
+        </Link>
+      </h1>
+      <ul className="mainRoutes">
+        {navRootList.map((r) => (
+          <NavItem route={r} currentPath={currentPath} key={r.key} />
+        ))}
+      </ul>
+    </aside>
+  );
+};
+
+export default Nav;
