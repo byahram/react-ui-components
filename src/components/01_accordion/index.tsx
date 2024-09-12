8348b44725fc37a8245735b6fb7560982c7254ba
Ahram Kim <45866137+byahram@users.noreply.github.com>
2024-09-12 16:44:57 +0900

    아코디언 (2/6) css로 숨김/보임 처리

--- src/components/01_accordion/index.tsx
+++ src/components/01_accordion/index.tsx
@@ -1,10 +1,12 @@
 import Accordion1 from "./1_r";
+import Accordion2 from "./2_r";
 import cx from "./cx";
 
 const Accordions = () => {
   return (
     <div className={cx("Accordions")}>
       <Accordion1 />
+      <Accordion2 />
     </div>
   );
 };
